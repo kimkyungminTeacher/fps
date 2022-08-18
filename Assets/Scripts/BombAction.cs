1df76f6f6df4f004dce09d392e06e5bdb06936de
kimkyungmin <kimkyungmin@sonline20.sen.go.kr>
2022-08-18 09:45:24 +0900

    chapter 4.3

---
+++ Assets/Scripts/BombAction.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombAction : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision other) {
+        Destroy(gameObject);
+    }
+}
